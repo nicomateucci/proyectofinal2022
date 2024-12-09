5208bb59e7c9a8cf396816d6c11c156c25d19793
Gonzalo Castillo <gonzalo07.xc@gmail.com>
2024-12-09 14:20:28 -0300

    GC; Add Logic for elements pagination

--- FrontEnd/src/app/UI-Componets/list-courses/list-courses.component.ts
+++ FrontEnd/src/app/UI-Componets/list-courses/list-courses.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input, ViewChild } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { CourseService } from 'src/app/Services/courses/course.service';
 import { UserService } from 'src/app/Services/users/user.service';
@@ -14,11 +14,12 @@ export class ListCoursesComponent {
   cursos !:any;
   TempListCourses !: any;
   panelOpenState = false;
+  pageSize!: number;
+  pageSizeOptions!: number[];
 
   constructor(
     private coursesService: CourseService,
     public userService: UserService,
-    private cdr : ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -26,31 +27,30 @@ export class ListCoursesComponent {
   }
 
   getAllCourses(){
+    this.updatePageSize();
     //se deberia chequear el usuario si esta logeado o no, para devolver todos los cursos segun sea la vista de cursos o mis cursos
     this.coursesService.getAllCourses().subscribe(
       (data : any) =>{
         this.cursos = data ;
-        this.TempListCourses = data.slice(0,6);
-      });
-      this.cdr.detectChanges();
+        this.TempListCourses = data.slice(0,this.pageSize);
+    });
   }
 
   getCoursesByLevel(){
+    this.updatePageSize();
     this.coursesService.getCourses(this.level).subscribe(
       (data : any) =>{
         this.cursos = data ;
       })
-      //Para paginar lso primeros 6 cursos
-      this.TempListCourses = this.cursos.slice(0,6);
-      this.cdr.detectChanges();
+      this.TempListCourses = this.cursos.slice(0,this.pageSize);
   }
 
   showMessage() {
     alert('Curso Agregado!')
   }
 
-  handlePageEvent(e: PageEvent) {
-    this.TempListCourses = this.cursos.slice(e.pageIndex * e.pageSize, (e.pageIndex + 1) * e.pageSize);
+  handlePageEvent(event: PageEvent) {
+    this.updateDisplayedCourses(event.pageIndex,event.pageSize)
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
@@ -58,6 +58,35 @@ export class ListCoursesComponent {
     if(this.cursos != undefined){
       return this.cursos.length
     }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.updatePageSize(); 
+  }
+
+  updatePageSize(): void {
+    switch (true) { 
+      case (window.innerWidth <= 600):
+        this.pageSize = 6;
+        this.pageSizeOptions = [6];
+        break;
+      case (window.innerWidth > 600 && window.innerWidth <= 1024):
+        this.pageSize = 4;
+        this.pageSizeOptions = [4,8,12];
+        break;
+      default:
+        this.pageSize = 8;
+        this.pageSizeOptions = [8,12,16];
+        break;
+    }
+    if (this.cursos) {
+      this.updateDisplayedCourses(0,this.pageSize)
+    }
+  }
+
+  updateDisplayedCourses(pageIndex: number, pageSize:number): void {
+    this.TempListCourses = this.cursos.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize);
   }
 
 }
